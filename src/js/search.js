80e976f983be1eb8b00d64ba9a55b47090b25bc4
Kristof Buts <kr.buts@gmail.com>
2019-06-03 15:32:53 +0200

    Search

--- src/js/search.js
+++ src/js/search.js
@@ -4,9 +4,13 @@ import {Customer} from "./Customer";
 
 export function loadSearch() {
 	loadCustomers();
+	document.querySelector("#search-input").addEventListener(
+		"input", e => loadCustomers(e.target.value.toLowerCase()), false
+	)
 }
 
-function loadCustomers() {
+function loadCustomers(query = "") {
+	clearTable();
 	getCustomers()
 		.then(result => {
 			let customers = [];
@@ -16,11 +20,29 @@ function loadCustomers() {
 					c.callsToServiceLine, c.registrationDate, c.isActive
 				)
 			));
+
+			if (query) { // only if query not null, undefined or ""
+				customers = customers
+					.filter(c =>
+						c.firstName.toLowerCase().includes(query)
+						|| c.lastName.toLowerCase().includes(query)
+						|| c.company.toLowerCase().includes(query)
+					);
+			}
+
 			addCustomersToTable(customers);
 		})
 		.catch(e => {
 			console.error(e);
 		});
+}
+
+function clearTable() {
+	// faster than innerHTML = ''
+	let element = document.querySelector("#customer-table");
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
 }
 
 function addCustomersToTable(customers) {
@@ -41,3 +63,4 @@ function addCustomerToTable(customer, body) {
 		`;
 	body.appendChild(row);
 }
+
